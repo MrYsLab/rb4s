30b50d6de9ae70117c1aab010008fe45a52983bc
Alan Yorinks <MisterYsLab@gmail.com>
2015-08-21 20:00:20 -0400

    Moved file to top level directory

--- rb4s.js
+++ rb4s.js
@@ -11,7 +11,7 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.my_first_block = function() {
+    ext.motor_control = function() {
         // Code that gets executed when the block is run
     };
 
@@ -19,10 +19,38 @@
     var descriptor = {
         blocks: [
             // Block type, block name, function name
-            [' ', 'my second block', 'my_second_block'],
-        ]
+            [' ', 'Motor Control motor: %m.motor operation: %m.operation', 'motor_control'],
+        ],
+        menus: {
+            motor: ['left', 'right'],
+            operation: ['forward', 'reverse', 'coast', 'brake']
+        },
+        url: 'http://scratchx.org/?url=http://MrYsLab.github.io/rb4s'
     };
 
     // Register the extension
     ScratchExtensions.register('RedBot For Scratch', descriptor, ext);
 })({});
+
+
+/*
+var descriptor = {
+    blocks: [
+        ['w', 'turn motor on for %n secs',             'motorOnFor', 1],
+        [' ', 'turn motor on',                         'allMotorsOn'],
+        [' ', 'turn motor off',                        'allMotorsOff'],
+        [' ', 'set motor power %n',                    'startMotorPower', 100],
+        [' ', 'set motor direction %m.motorDirection', 'setMotorDirection', 'this way'],
+        ['h', 'when distance %m.lessMore %n',          'whenDistance', '<', 20],
+        ['h', 'when tilt %m.eNe %n',                   'whenTilt', '=', 1],
+        ['r', 'distance',                              'getDistance'],
+        ['r', 'tilt',                                  'getTilt']
+    ],
+    menus: {
+        motorDirection: ['this way', 'that way', 'reverse'],
+        lessMore: ['<', '>'],
+        eNe: ['=','not =']
+    },
+    url: 'http://info.scratch.mit.edu/WeDo'
+};
+ */
