dc6be507d8417a0d7d9bfb0c23a0e4b409fb0e51
Alan Yorinks <MisterYsLab@gmail.com>
2015-08-22 11:08:55 -0400

    added redbot blocks

--- rb4s.js
+++ rb4s.js
@@ -68,7 +68,7 @@
             [' ', 'Set brake for %m.motor motor',                           'brake', 'Left'],
             [' ', 'LED 13 %m.ledState',                                     'ledCcontrol' ,'On'],
             [' ', 'Play Tone  %n Hz  %n ms',                                'playTone', '1000', '500'],
-            [' ', 'Reset Encoder Count %n',                                    'resetCount', '0'],
+            [' ', 'Reset Encoder Count',                                    'resetCount'],
             ['h', 'When User Button Is Pushed',                             'hatPushButton'],
             ['h', 'When %m.bumper bumper activates',                        'bumpers', 'Left'],
             ['h', 'When tap sensor activates',                              'tap'],
