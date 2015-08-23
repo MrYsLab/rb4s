8f7290ae790127f5a11db6945703ca360a94e098
Alan Yorinks <MisterYsLab@gmail.com>
2015-08-23 17:09:55 -0400

    added redbot blocks

--- rb4s.js
+++ rb4s.js
@@ -3,7 +3,7 @@
  */
 (function (ext) {
 
-    console.log('rb4sx.js alpha_016');
+    console.log('rb4sx.js alpha_017');
     // 0 = no debug
     // 1 = low level debug
     // 2 = high - open the floodgates
