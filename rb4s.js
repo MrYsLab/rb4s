0ae8aac59c698a4d94c45d92b751db930f25651f
Alan Yorinks <MisterYsLab@gmail.com>
2015-08-26 08:59:48 -0400

    code cleanup

--- rb4s.js
+++ rb4s.js
@@ -3,7 +3,7 @@
  */
 (function (ext) {
 
-    console.log('rb4sx.js alpha_042');
+    console.log('rb4sx.js alpha_043');
     // 0 = no debug
     // 1 = low level debug
     // 2 = high - open the floodgates
@@ -143,7 +143,7 @@
 
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function (myStatus, myMsg) {
+    ext._getStatus = function (status, msg) {
         return {status: myStatus, msg: myMsg};
     };
 
